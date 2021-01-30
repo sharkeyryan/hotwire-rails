6c3144686a87a40df9c18cf93ab59700234e6b87
Ryan Sharkey <rsharkey@kloveair1.com>
2021-01-29 23:15:02 -0800

    Setup DB and add rich text content filed to Tweet form

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets or /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(created_at: :desc)
   end
 
   # GET /tweets/1 or /tweets/1.json
@@ -64,6 +64,6 @@ class TweetsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def tweet_params
-      params.fetch(:tweet, {})
+      params.fetch(:tweet).permit(:content)
     end
 end
